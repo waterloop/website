df5b4c97ad29899575f2a1161c78ca610310e6fe
Taehyun Lee <taehyunlee4482@gmail.com>
2019-06-22 14:08:36 -0400

    made fixes to teamStructureS19.js

--- routes/teamStructureS19.js
+++ routes/teamStructureS19.js
@@ -1,78 +1,78 @@
 module.exports = {
-  "Technical": {
-    "Chawthri Kanagarasa": "Technical Director",
-    "Benjamin Li": "Technical Project Manager",
-    "Pascal Voyer-Nguyen": "Integration Lead / Co-op Supervisor",
-    "Electrical": {
-      "Arhan Banerjee": "Member",
-      "Christina Sullivan ": "Member",
-      "Connor McCallum": "Member",
-      "Elaine Rennick": "Member",
-      "Felix Suen": "Member",
-      "Jordan Lau": "Member",
-      "Kelvin Tezinde": "Member",
-      "Linda Mao": "Member",
-      "Luke Doria": "Member",
-      "Lynn Kim": "Member",
-      "Ryun Kim": "Member",
-      "David Liu": "Electrical Lead"
+    "Technical": {
+        "Chawthri Kanagarasa": "Technical Director",
+        "Benjamin Li": "Technical Project Manager",
+        "Pascal Voyer-Nguyen": "Integration Lead",
+        "Electrical": {
+            "David Liu": "Lead",
+            "Arhan Banerjee": "",
+            "Christina Sullivan ": "",
+            "Connor McCallum": "",
+            "Cynthia Wang": "",
+            "Elaine Rennick": "",
+            "Felix Suen": "",
+            "Jordan Lau": "",
+            "Kelvin Tezinde": "",
+            "Linda Mao": "",
+            "Luke Doria": "",
+            "Lynn Kim": "",
+            "Ryun Kim": ""
+        },
+        "Mechanical": {
+            "Donovan Kwong": "R&D Lead",
+            "Kenton Tran": "Manufacturing Lead",
+            "Angelito Elipse": "",
+            "Arnav Garcha": "",
+            "Ben Cohen": "",
+            "Bryan Poon": "",
+            "Clara Tsai": "",
+            "Jennifer Chen": "",
+            "Steven Phang": "Co-op"
+        },
+        "Software": {
+            "Deep Dhillon": "Lead",
+            "Alison Luo": "",
+            "Felix Wong": "",
+            "Ramnik Chudha": "",
+            "Roman Olynyk": "",
+            "Wais Shahbaz": "",
+            "Yogesh Aggarwal": ""
+        }
     },
-    "Mechanical": {
-      "Angelito Elipse": "Member",
-      "Ben Cohen": "Member",
-      "Bryan Poon": "Member",
-      "Clara Tsai": "Member",
-      "Cynthia Wang": "Member",
-      "Steven Phang": "Member",
-      "Kenton Tran": "Mechanical - Manufacturing Lead",
-      "Donovan Kwong": "Mechanical - R&D Lead",
-      "Jennifer Chen": "Mechanical / Co-op Supervisor",
-      "Arnav Garcha": "Member"
+    "Admin": {
+        "Zahra Kamal": "Administrative Director",
+        "Jason Pan": "Advisor",
+        "Marketing": {
+            "Anton Chow": "",
+            "Joanne Wong": ""
+        },
+        "Sponsorship": {
+            "Soham Suvarnkar": "Lead",
+            "John Glennie": "",
+            "Reena Patel": "Co-op",
+            "Rose Yang": "Co-op"
+        },
+        "Finance": {
+            "Jhalina Visvalingam": "Lead",
+            "Nipinder Ghuman": "",
+            "Palvit Garg": ""
+        },
+        "Infrastructure": {
+            "Candy Lee": "",
+            "Yuvna Apajee": ""
+        },
+        "Web Design": {
+            "Anushka Tayal": "Co-lead",
+            "Erin Ly": "Co-lead",
+            "Hariharan Karthikeyan": "",
+            "Siddharth Garg ": "",
+            "Taehyun Lee": "",
+            "Tim He": ""
+        }
     },
-    "Software": {
-        "Alison Luo": "Member",
-        "Felix Wong": "Member",
-        "Ramnik Chudha": "Member",
-        "Roman Olynyk": "Member",
-        "Wais Shahbaz": "Member",
-        "Yogesh Aggarwal": "Member",
-        "Deep Dhillon": "Software Lead"
-    },
-  },
-  "Admin": {
-    "Zahra Kamal" :"Administrative Director",
-    "Jason Pan": "Advisor / Co-op Supervisor",
-    "Marketing": {
-      "Anton Chow": "Member",
-      "Joanne Wong": "Member"
-    },
-    "Sponsorship": {
-        "John Glennie": "Member",
-        "Reena Patel": "Member",
-        "Rose Yang": "Member",
-        "Soham Suvarnkar": "Sponsorship Lead"
-    },
-    "Finance": {
-        "Palvit Garg": "Member",
-        "Jhalina Visvalingam": "Finance Lead",
-        "Nipinder Ghuman": "Member"
-    },
-    "Infrastructure": {
-        "Candy Lee": "Member",
-        "Yuvna Apajee": "Member"
-    },
-    "Web Design": {
-      "Hariharan Karthikeyan": "Member",
-      "Siddharth Garg ": "Member",
-      "Taehyun Lee": "Member",
-      "Tim He": "Member",
-      "Anushka Tayal": "Web Design Co-lead",
-      "Erin Ly": "Web Design Co-lead"
-    },
-  },
-  "Advisor": {
-    "Dallyn Wynnychuk": "Advisor",
-    "Emrys Halbertsma": "Advisor",
-    "Jimmy Zhou": "Advisor"
-  },
+    "Advisor": {
+        "Dallyn Wynnychuk": "Advisor",
+        "Emrys Halbertsma": "Advisor",
+        "Jimmy Zhou": "Advisor"
+    }
 }
